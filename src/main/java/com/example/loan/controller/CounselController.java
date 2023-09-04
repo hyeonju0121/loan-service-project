dca0e036a0ae0aa9739786d22ce8e60ea4a3a185
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-09-04 18:56:46 +0900

    Feat : 대출 상담 조회 api 구현

--- src/main/java/com/example/loan/controller/CounselController.java
+++ src/main/java/com/example/loan/controller/CounselController.java
@@ -4,10 +4,7 @@ import com.example.loan.dto.CounselDTO;
 import com.example.loan.dto.ResponseDTO;
 import com.example.loan.service.CounselService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +14,13 @@ public class CounselController extends AbstractController {
     private final CounselService counselService;
 
     @PostMapping
-    public ResponseDTO<CounselDTO.Response> create(
+    public ResponseDTO<CounselDTO.Response> createCounsel(
             @RequestBody CounselDTO.Request request) {
         return ok(counselService.create(request));
+    }
+
+    @GetMapping("/{counselId}")
+    public ResponseDTO<CounselDTO.Response> getCounsel(@PathVariable Long counselId) {
+        return ok(counselService.getCounsel(counselId));
     }
 }
--- src/main/java/com/example/loan/service/CounselServiceImpl.java
+++ src/main/java/com/example/loan/service/CounselServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.loan.service;
 
 import com.example.loan.domain.Counsel;
 import com.example.loan.dto.CounselDTO;
+import com.example.loan.exception.BaseException;
+import com.example.loan.exception.ResultType;
 import com.example.loan.repository.CounselRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -22,5 +24,13 @@ public class CounselServiceImpl implements CounselService {
         Counsel created = counselRepository.save(counsel);
 
         return modelMapper.map(created, CounselDTO.Response.class);
+    }
+
+    @Override
+    public CounselDTO.Response getCounsel(Long counselId) {
+        Counsel counsel = counselRepository.findById(counselId)
+                .orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        return modelMapper.map(counsel, CounselDTO.Response.class);
     }
 }
