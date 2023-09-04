b2a366e570d4ff500844ba048152cb5d28f43740
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-09-04 18:57:18 +0900

    Test : 대출 상담 조회 서비스 테스트 코드 작성

--- src/test/java/com/example/loan/service/CounselServiceTest.java
+++ src/test/java/com/example/loan/service/CounselServiceTest.java
@@ -1,10 +1,14 @@
 package com.example.loan.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.loan.domain.Counsel;
 import com.example.loan.dto.CounselDTO;
+import com.example.loan.exception.BaseException;
+import com.example.loan.exception.ResultType;
 import com.example.loan.repository.CounselRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +19,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class CounselServiceTest {
@@ -60,5 +66,42 @@ public class CounselServiceTest {
 
         //then
         assertThat(response.getName()).isSameAs(entity.getName());
+    }
+
+    @Test
+    @DisplayName("대출 상담 조회 서비스")
+    void Should_ReturnResponseOfExistsCounselEntity_When_RequestExistCounselId() {
+        //given
+        Long counselId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .build();
+
+        when(counselRepository.findById(counselId))
+                .thenReturn(Optional.ofNullable(entity));
+
+        //when
+        CounselDTO.Response response = counselService.getCounsel(counselId);
+
+        //then
+        assertThat(response.getCounselId()).isSameAs(counselId);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상담 아이디인 경우 - 대출 상담 조회 실패")
+    void Should_ThrowException_When_RequestNotExistCounselId() {
+        //given
+        Long counselId = 1L;
+
+        when(counselRepository.findById(counselId))
+                .thenThrow(new BaseException(ResultType.SYSTEM_ERROR));
+
+        //when
+        BaseException exception = assertThrows(BaseException.class,
+                () -> counselService.getCounsel(counselId));
+
+        //then
+        assertEquals(ResultType.SYSTEM_ERROR, exception.getErrorCode());
     }
 }
