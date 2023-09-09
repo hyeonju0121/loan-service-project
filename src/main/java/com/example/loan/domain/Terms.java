f54cfef09b1af00e17c43bb3088c5a23f761631f
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-09-09 18:46:16 +0900

    Feat : Terms Entity & Repository 생성

---
+++ src/main/java/com/example/loan/domain/Terms.java
@@ -0,0 +1,30 @@
+package com.example.loan.domain;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Where(clause = "is_deleted=false")
+public class Terms extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long termsId;
+
+    @Column(columnDefinition = "varchar(255) COMMENT '약관 이름'")
+    private String name;
+
+    @Column(columnDefinition = "varchar(255) COMMENT '약관상세 URL'")
+    private String termsDetailUrl;
+}
