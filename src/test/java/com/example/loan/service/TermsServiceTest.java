2cf75b626334256b62dfd3bcdd4a617fb158a8c8
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-09-09 19:23:15 +0900

    Test : 이용 약관 전체 조회 서비스 테스트 코드 작성

--- src/test/java/com/example/loan/service/TermsServiceTest.java
+++ src/test/java/com/example/loan/service/TermsServiceTest.java
@@ -13,6 +13,10 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -52,5 +56,30 @@ public class TermsServiceTest {
         assertThat(response.getTermsDetailUrl()).isEqualTo(entity.getTermsDetailUrl());
     }
 
+
+    @Test
+    @DisplayName("약관 전체 조회 서비스")
+    void Should_ReturnAllResponseOfExistTermsEntities_When_RequestTermsList() {
+        //given
+        Terms entityA = Terms.builder()
+                        .name("대출 이용약관 1")
+                        .termsDetailUrl("http://testtest.asfddf/sdfd")
+                        .build();
+        Terms entityB = Terms.builder()
+                .name("대출 이용약관 2")
+                .termsDetailUrl("http://testtest.asfddf/sdfdfffdfd")
+                .build();
+
+        List<Terms> termsList = new ArrayList<>(Arrays.asList(entityA, entityB));
+
+        when(termsRepository.findAll())
+                .thenReturn(termsList);
+
+        //when
+        List<TermsDTO.Response> response = termsService.getAllTerms();
+
+        //then
+        assertThat(response.size()).isEqualTo(termsList.size());
+    }
 
 }
