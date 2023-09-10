723d8d720900153978f387cc77a4ad3accd6984a
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-09-10 17:23:43 +0900

    Feat : 대출 심사 조회 기능 구현

--- src/main/java/com/example/loan/exception/ResultType.java
+++ src/main/java/com/example/loan/exception/ResultType.java
@@ -12,6 +12,8 @@ public enum ResultType {
 
     NOT_FOUND_APPLICATION("9000", "대출 신청 내역이 존재하지 않습니다."),
 
+    NOT_FOUND_JUDGMENT("9000", "대출 심사 내역이 존재하지 않습니다."),
+
     NOT_ACCEPT_ALL_TERMS("9000", "모든 약관에 동의하지 않았습니다."),
 
     ACCEPT_NOT_EXIST_TERMS("9000", "존재하지 않는 약관에 동의했습니다."),
