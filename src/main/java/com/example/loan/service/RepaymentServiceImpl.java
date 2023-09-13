75ca50d7573ba5e9c0dbcc3f2444af9a91525d3c
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-09-13 22:55:12 +0900

    Feat : 대출 상환 기능 구현

---
+++ src/main/java/com/example/loan/service/RepaymentServiceImpl.java
@@ -0,0 +1,78 @@
+package com.example.loan.service;
+
+import com.example.loan.domain.Application;
+import com.example.loan.domain.Entry;
+import com.example.loan.domain.Repayment;
+import com.example.loan.dto.BalanceDTO;
+import com.example.loan.dto.RepaymentDTO;
+import com.example.loan.exception.BaseException;
+import com.example.loan.exception.ResultType;
+import com.example.loan.repository.ApplicationRepository;
+import com.example.loan.repository.EntryRepository;
+import com.example.loan.repository.RepaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RepaymentServiceImpl implements RepaymentService {
+
+    private final RepaymentRepository repaymentRepository;
+
+    private final ApplicationRepository applicationRepository;
+
+    private final EntryRepository entryRepository;
+
+    private final BalanceService balanceService;
+
+    private final ModelMapper modelMapper;
+
+    /**
+     * 대출금 상환
+     */
+    @Override
+    public RepaymentDTO.Response create(Long applicationId, RepaymentDTO.Request request) {
+        // validation
+        if (!isRepayableApplication(applicationId)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        Repayment repayment = modelMapper.map(request, Repayment.class);
+        repayment.setApplicationId(applicationId);
+
+        repaymentRepository.save(repayment);
+
+        // 잔고 업데이트
+        BalanceDTO.Response updatedBalance = balanceService.repaymentUpdate(applicationId,
+                BalanceDTO.RepaymentRequest.builder()
+                        .type(BalanceDTO.RepaymentRequest.RepaymentType.REMOVE)
+                        .repaymentAmount(request.getRepaymentAmount())
+                        .build());
+
+        RepaymentDTO.Response response =
+                modelMapper.map(repayment, RepaymentDTO.Response.class);
+        // 남은 대출금 잔액
+        response.setBalance(updatedBalance.getBalance());
+
+        return response;
+    }
+
+    private boolean isRepayableApplication(Long applicationId) {
+
+        Optional<Application> application =
+                applicationRepository.findById(applicationId);
+
+        // 대출 신청 존재여부 검증
+        if (application.isEmpty()) return false;
+
+        // 계약을 완료한 신청 정보인지
+        if (application.get().getContractedAt() == null) return false;
+
+        // 집행이 완료된 신청 정보인지
+        Optional<Entry> entry = entryRepository.findByApplicationId(applicationId);
+        return entry.isPresent();
+    }
+}
